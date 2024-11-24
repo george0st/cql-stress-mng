fecfac0e22786684d184c27d02a7ee06d3a34d0e
jist <95856749+george0st@users.noreply.github.com>
2024-11-24 13:11:53 +0100

    Update stress_run.sh

--- stress/helper/stress_run.sh
+++ stress/helper/stress_run.sh
@@ -6,7 +6,7 @@
 
 python3.11 stress/stress_mng.py run -e _cass_v4_*.env -l ../_cass_v4.sh -d stress
 python3.11 stress/stress_mng.py run -e _cass_v5_*.env -l ../_cass_v5.sh -d stress
-python3.11 stress/stress_mng.py run -e _cass_v5_*.env -l ../_cass_all.sh -d stress
+python3.11 stress/stress_mng.py run -e _cass_*.env -l ../_cass_all.sh -d stress
 
 nohup ./_cass_v4.sh
 nohup ./_cass_v5.sh
