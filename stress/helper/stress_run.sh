726df4ce7a17fccbfd19ea89ddb62cf9fd41fbc3
jist <95856749+george0st@users.noreply.github.com>
2024-11-24 18:44:13 +0100

    ...

--- stress/helper/stress_run.sh
+++ stress/helper/stress_run.sh
@@ -4,6 +4,7 @@
 # Run without terminal
 ###########################
 
+# Mains
 python3.11 stress/stress_mng.py run -e _cass_v4_*.env -l ../_cass_v4.sh -d stress
 python3.11 stress/stress_mng.py run -e _cass_v5_*.env -l ../_cass_v5.sh -d stress
 python3.11 stress/stress_mng.py run -e _cass_*.env -l ../_cass_all.sh -d stress
@@ -11,3 +12,15 @@ python3.11 stress/stress_mng.py run -e _cass_*.env -l ../_cass_all.sh -d stress
 nohup ./_cass_v4.sh
 nohup ./_cass_v5.sh
 nohup ./_cass_all.sh
+
+# Others
+python3.11 stress/stress_mng.py run -e _cass_v4_1*.env -l ../_cass_v4_1.sh -d stress
+python3.11 stress/stress_mng.py run -e _cass_v4_2*.env -l ../_cass_v4_2.sh -d stress
+python3.11 stress/stress_mng.py run -e _cass_v5_1*.env -l ../_cass_v5_1.sh -d stress
+python3.11 stress/stress_mng.py run -e _cass_v5_2*.env -l ../_cass_v5_2.sh -d stress
+
+nohup ./_cass_v4_1.sh
+nohup ./_cass_v4_2.sh
+nohup ./_cass_v5_1.sh
+nohup ./_cass_v5_2.sh
+
