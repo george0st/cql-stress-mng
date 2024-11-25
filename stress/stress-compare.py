86b02a8ee93e07fb54afe759d1ed44c7e97e9cb8
jist <95856749+george0st@users.noreply.github.com>
2024-11-25 13:29:13 +0100

    Init StressOutput

---
+++ stress/stress-compare.py
@@ -0,0 +1,10 @@
+
+
+
+class StressCompare:
+    """ Generate compare stress tests (typically between cassandra v4 and v5).
+        The outputs are in json, txt format
+    """
+
+    def __init__(self):
+        pass
