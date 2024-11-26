7c47f0620133117d889e3fbf71c1f7dd013ff373
jist <95856749+george0st@users.noreply.github.com>
2024-11-26 16:55:09 +0100

    Update stress_summary.py

--- stress/stress_summary.py
+++ stress/stress_summary.py
@@ -14,19 +14,23 @@ class StressSummary:
         self._output_dir = output_dir
         self._file_extension = file_extension
 
-    def _parse_content(self, file_name, content) -> dict:
+    def _parse_content(self, file_name, content) -> (str,dict):
 
         output={}
+
+        #file_name = "2024-11-25_22-02-41 v5 read_LOCAL_ONE_UCS10_32xTHR.txt"
+        key = self._get_pattern_item("(.*)_[^_]*xTHR.txt$", file_name)
 
         # basic info
         output["operation_type"]=self._get_pattern_item(f"Type:(.*)$", content)
         output["duration"] = self._get_pattern_item(f"Duration:(.*)$", content)
         output["consistency_level"] = self._get_pattern_item(f"Consistency Level:(.*)$", content)
-        output["executors"] = self._get_pattern_item("Thread Count:(.*)$", content)
+        output["executors"] = int(self._get_pattern_item("Thread Count:(.*)$", content))
 
         # optional (can be zero for read operation)
-        output["compaction"] = self._get_pattern_item("Table Compaction Strategy:(.*)$", content)
-        output["compaction_option"] = self._get_pattern_item("Table Compaction Strategy Options:(.*)$", content)
+        #file_name = "2024-11-25_22-02-41 v5 read_LOCAL_ONE_LCS_64xTHR.txt"
+        #file_name = "2024-11-25_22-02-41 v5 read_LOCAL_ONE_UCS10_32xTHR.txt"
+        output["compaction"] = self._get_pattern_item("_([^_]*)_[^_]*xTHR.txt$", file_name)
 
         # performance details
         output["performance"] = self._get_pattern_item("Op rate.+:(.+) op\/s  \[.+\]", content).replace(",","")
@@ -36,7 +40,7 @@ class StressSummary:
         output["latency_99th"] = self._get_pattern_item("Latency 99th percentile.+:(.+)\[.+\]", content).replace(",","")
         output["latency_999th"] = self._get_pattern_item("Latency 99.9th percentile.+:(.+)\[.+\]", content).replace(",","")
         output["latency_max"] = self._get_pattern_item("Latency max.+:(.+)\[.+\]", content).replace(",", "")
-        return output
+        return key, output
 
     def _get_pattern_item(self, pattern, content):
         match=re.findall(pattern, content, re.MULTILINE)
@@ -48,13 +52,25 @@ class StressSummary:
     def parse(self):
 
         performance = {}
+        items=[]
         # iteration cross all files
         for file_name in glob(path.join(self._output_dir, self._file_extension)):
             print("Processing:",file_name)
-            performance = self._parse_content(file_name, helper.read_file_all(file_name))
 
-            print(performance)
+            key, one_output = self._parse_content(path.basename(file_name), helper.read_file_all(file_name))
+            # file_name = "2024-11-25_22-02-41 v5 read_LOCAL_ONE_UCS10_32xTHR.txt"
+            # key = 2024-11-25_22-02-41 v5 read_LOCAL_ONE_UCS10
 
-            # output JSON for qgate-graph
+            # update items
+            if performance.get(key, None):
+                performance[key].append(one_output)
+            else:
+                items=[]
+                items.append(one_output)
+                performance[key]=items
 
+
+        # order by amount of executors
+
+        # output JSON for qgate-graph
 
