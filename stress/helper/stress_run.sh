f4d205b13f2fcd9c4f98214bdadcde8c9a2db091
jist <95856749+george0st@users.noreply.github.com>
2025-01-28 13:44:59 +0100

    Extend graph compare

--- stress/helper/stress_run.sh
+++ stress/helper/stress_run.sh
@@ -8,7 +8,6 @@ nohup ./_zeebe_all.sh
 
 
 python3.11 stress/stress_mng.py generate -e private/zeebe/_cass_*.env -l ../_zeebe_all.sh -d stress
-
 ./_zeebe_all.sh
 
 # Mains for 'compareV4V5_sequenceTHR'
